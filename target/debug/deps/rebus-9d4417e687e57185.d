f794d4933ecacf1b781f92d7e4da9e25e724a3d6
kelseyde <daniel.kelsey@hotmail.co.uk>
2025-01-30 15:51:53 +0100

    Update gitignore

--- target/debug/deps/rebus-9d4417e687e57185.d
+++
@@ -1,9 +0,0 @@
-/Users/kelseyde/git/dan/rebus/target/debug/deps/rebus-9d4417e687e57185: src/main.rs src/board.rs src/consts.rs src/bits.rs src/moves.rs
-
-/Users/kelseyde/git/dan/rebus/target/debug/deps/rebus-9d4417e687e57185.d: src/main.rs src/board.rs src/consts.rs src/bits.rs src/moves.rs
-
-src/main.rs:
-src/board.rs:
-src/consts.rs:
-src/bits.rs:
-src/moves.rs:
